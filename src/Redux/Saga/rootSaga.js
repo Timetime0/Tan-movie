d735684c58570090c1de8525b91ef4518365b152
Timetime0 <75958612+Timetime0@users.noreply.github.com>
2021-05-26 11:51:54 +0700

    Publish fist static website

--- src/Redux/Saga/rootSaga.js
+++ src/Redux/Saga/rootSaga.js
@@ -16,9 +16,6 @@ export function * rootSaga(){
         dataPhimSaga.theoDoiActionUpdateImgDataPhimApi(),
         dataPhimSaga.theoDoiActionAddImgPhimApi(),
 
-
-
-
         dataRapPhimSaga.theoDoiAcTionGetDataRapPhimApi(),
         dataRapPhimSaga.theoDoiActionGetDataCumRapPhimApi(),
         
